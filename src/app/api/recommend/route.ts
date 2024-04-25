03673b58679830de82853a782f3c6e64e8b01705
twentytwo44 <95289695+Twentytwo44@users.noreply.github.com>
2024-04-25 21:13:00 +0700

    commit

--- src/app/api/recommend/route.ts
+++ src/app/api/recommend/route.ts
@@ -1,1 +1,25 @@
 'use server'
+import { response } from 'express'
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
+type Props = {}
+
+const route = (props: Props) => {
+
+    const [userID, setUserID] = useState('')
+
+    const PostUserID = async () => {
+        axios.post('http://localhost:3000/api/recommend', userID)
+        .then(response => {
+            console.log(response)
+        })
+    }
+
+    
+
+    return Response.json({
+        name:'hello world!'
+    })
+}
