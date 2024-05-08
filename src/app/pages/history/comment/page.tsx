67276d875b97ebca44f632c7fa91a70af4109b16
twentytwo44 <95289695+Twentytwo44@users.noreply.github.com>
2024-05-08 20:04:10 +0700

    cm

--- src/app/pages/history/comment/page.tsx
+++ src/app/pages/history/comment/page.tsx
@@ -8,6 +8,7 @@ const page = (props: Props) => {
     <>
      <div className='w-full flex justify-center mt-[50px] max-sm:mt-[10px] '>
         <div className='w-[1140px] flex flex-col gap-4 p-3'>
+          <h1 className='text-3xl text-[#202192] font-bold'>ให้คะแนนความพึงพอใจ</h1>
        
         </div>
     </div>
