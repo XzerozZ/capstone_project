6df0e95a9461a2bdb6248e912a5d3962cccbecc6
XzerozZ <111548296+XzerozZ@users.noreply.github.com>
2024-05-24 03:06:45 +0700

    register for company

---
+++ src/app/api/auth/registercom/route.ts
@@ -0,0 +1,58 @@
+'use server'
+import bcrypt from 'bcrypt';
+import prisma from '../../utils/prisma';
+import { formatIdcard , formatPhoneNumber } from '../format';
+import { upLoadIMG } from '../../admin/supabase';
+
+export async function POST(req: Request) {
+    try {
+        const formData = await req.formData();
+        let image: string | null = null;
+        for await (const [name, value] of formData.entries()) {
+            if (name === 'image' && value instanceof File) {
+                image = await upLoadIMG(value);
+            } 
+            else if (name === 'phone_number') {
+                const formatPhone = await formatPhoneNumber(value as string);
+                formData.set(name, formatPhone);
+            }
+            else if (name === 'password') {
+                const hashedPassword = bcrypt.hashSync(value as string, 10);
+                formData.set(name, hashedPassword);
+            }
+        }
+        const email = formData.get('email') as string
+        const user = await prisma.user.findUnique({
+            where : {
+                email : email
+            }
+        })
+        if(!user){
+            const newuser = await prisma.user.create({
+                data: {
+                    username: formData.get('username') as string,
+                    image: image,
+                    phone_number: formData.get('phone_number') as string, // 1234567890
+                    email: email,
+                    password: formData.get('password') as string,
+                    role: formData.get('role') as string,
+                },
+            });
+            await prisma.$disconnect();
+            return Response.json(newuser);
+        }
+        else{
+            await prisma.$disconnect();
+            return Response.json({
+                message : "Email already used"
+            }, { status: 404 });
+        }
+        
+    } catch (error) {
+        await prisma.$disconnect();
+        console.log(error);
+        return Response.json({
+            error
+        }, { status: 500 });
+    }
+}
